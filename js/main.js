840d77066eaed3def85b178a5fab58195da9efd9
Oral <kdirbaevoral875@gmail.com>
2024-01-06 19:41:31 +0300

    localStorage

--- js/main.js
+++ js/main.js
@@ -3,64 +3,64 @@
 const cookieBox = document.querySelector(".popup");
 const btnAccept = document.getElementById("accept-btn");
 
-const executeCodes = () => {
-    cookieBox.classList.add("is-visible");
 
-    btnAccept.addEventListener("click", () => {
-        document.cookie = "cookieBy= RegionBeer; max-age=" + 60 * 60 * 48;
+document.addEventListener("DOMContentLoaded", function() {
+    // Проверяем, существует ли у клиента localStorage
+    if (typeof(Storage) !== "undefined") {
+      // Если localStorage существует, проверяем, сохранено ли определенное значение с ключом 'visited'
+      if (!localStorage.getItem('visited')) {
+        // Если значение не сохранено, показываем popup окно
+        // Здесь нужно добавить код для вашего popup окна
+        cookieBox.classList.add("is-visible");
+        // После того, как пользователь нажмет на кнопку в окне, мы устанавливаем значение в localStorage
+        document.getElementById('accept-btn').addEventListener('click', function() {
+          localStorage.setItem('visited', true);
+          var now = new Date().getTime();
+          var ttl = 48 * 60 * 60 * 1000; // 48 часов в миллисекундах
+          var exp = now + ttl;
+          localStorage.setItem('expiration', exp);
+            
         cookieBox.classList.remove("is-visible");
-    })
-}
-
-window.addEventListener("load", executeCodes);
-
-
-
-
-
-
-
-
-
-
-
+        });
+      }
+  }})
 
 /*  hamburger-menu */
 
-// const menu_btn = document.querySelector('.hamburger-menu');
-// const mobile_menu = document.querySelector('.header__menu');
+const menu_btn = document.querySelector('.hamburger-menu');
+const mobile_menu = document.querySelector('.header__menu');
 
-// menu_btn.addEventListener('click', function () {
-//     menu_btn.classList.toggle('is-active');
-//     mobile_menu.classList.toggle('is-active');
-// })
+menu_btn.addEventListener('click', function () {
+    menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+})
 
-// document.addEventListener("click", function (e) {
-//     if (!mobile_menu.contains(e.target) && !menu_btn.contains(e.target)) {
-//         menu_btn.classList.remove('is-active');
-//         mobile_menu.classList.remove('is-active');
-//     }
-// })
+document.addEventListener("click", function (e) {
+    if (!mobile_menu.contains(e.target) && !menu_btn.contains(e.target)) {
+        menu_btn.classList.remove('is-active');
+        mobile_menu.classList.remove('is-active');
+    }
+})
 
 /*   modal   */
 
-// const modalFunc = function () {
-//     const modal_btn = document.querySelector('.catalog-item__btn');
-//     const modal_window = document.querySelector('.order__modal-window');
-//     const modal_container = document.querySelector('.modal-window__container');
+const modalFunc = function () {
+    const modal_btn = document.querySelector('.catalog-item__btn');
+    const modal_window = document.querySelector('.order__modal-window');
+    const modal_container = document.querySelector('.modal-window__container');
 
-//     modal_btn.addEventListener('click', function () {
-//         modal_window.classList.add('is--visible');
-//     });
+    modal_btn.addEventListener('click', function () {
+        modal_window.classList.add('is--visible');
+    });
 
-//     document.querySelector('.exit').addEventListener('click', function () {
+    document.querySelector('.exit').addEventListener('click', function () {
 
-//         modal_window.classList.remove('is--visible');
+        modal_window.classList.remove('is--visible');
 
-//     });
-// }
+    });
+}
 
-// modalFunc();
+modalFunc();
 
 
 
